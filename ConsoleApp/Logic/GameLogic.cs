2fe610301791ef94ca016c34ab186f6672dc83fe
alexey8korolev <alexeykorolev75@gmail.com>
2024-05-18 23:45:58 +0300

    SOLID

---
+++ ConsoleApp/Logic/GameLogic.cs
@@ -0,0 +1,80 @@
+﻿using ConsoleApp.Enums;
+using ConsoleApp.Models;
+
+namespace ConsoleApp.Logic;
+
+public class GameLogic : IGameLogic
+{
+    private readonly IGame _curGame;
+
+    public GameLogic(IGame curGame)
+    {
+        _curGame = curGame;
+    }
+
+    public void Start()
+    {
+        Console.WriteLine();
+        Console.WriteLine("<<< Угадай число! >>>");
+        Console.WriteLine("--> Начинается новая игра... >>>");
+        Console.WriteLine();
+
+        _curGame.State = GameState.Play;
+    }
+
+    public void Play()
+    {
+        if (_curGame.State != GameState.Play)
+        {
+            Console.WriteLine(@"Игра ""Угадай число"" некорректное состояние");
+            return;
+        };
+
+        while (true)
+        {
+
+            Console.WriteLine(@$"Введите число в диапазоне от {_curGame.MinVal} до {_curGame.MaxVal}.
+Количество попыток равно {_curGame.Count}:              загадано {_curGame.Num}");
+
+            var curNumStr = Console.ReadLine()!;
+
+            if (curNumStr.Any(char.IsLetter))
+            {
+                Console.WriteLine($"Необходимо задать число!");
+                continue;
+            }
+
+            var curNum = Convert.ToInt32(curNumStr);
+
+            _curGame.Count--;
+
+            if (curNum == _curGame.Num)
+            {
+                _curGame.State = GameState.Win;
+                Console.WriteLine($"Вы выиграли!!! Правильно это число {curNum}");
+                break;
+
+            }
+
+            if (_curGame.Count > 0)
+            {
+                if (curNum > _curGame.Num)
+                {
+                    _curGame.State = GameState.More;
+                    Console.WriteLine($"Число {curNum} больше загаданного");
+                }
+                else
+                {
+                    _curGame.State = GameState.Less;
+                    Console.WriteLine($"Число {curNum} меньше загаданного");
+                }
+            }
+            else
+            {
+                _curGame.State = GameState.Fail;
+                Console.WriteLine($"Вы проиграли. Было загадано число {_curGame.Num}");
+                break;
+            }
+        }
+    }
+}
---
+++ ConsoleApp/Menu.cs
@@ -0,0 +1,44 @@
+﻿using ConsoleApp.Enums;
+using ConsoleApp.Logic;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ConsoleApp;
+
+public static class Menu
+{
+    public static void Show(IServiceProvider serviceProvider)
+    {
+        Console.WriteLine();
+        Console.WriteLine("<<< Играем в игру! >>>");
+        Console.WriteLine();
+
+        while (true)
+        {
+            var serviceGame = serviceProvider.GetService<IGameLogic>()!;
+
+            serviceGame.Start();
+            serviceGame.Play();
+
+            while (true)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Сыграем в игру еще раз (Д/Н) ?");
+
+                var cmd = Console.ReadLine()!.Trim().ToUpper();
+
+                if (cmd != "Д" && cmd != "Н")
+                {
+                    Console.WriteLine("Введите корректную команду : Д:(Да), Н:(Нет)");
+                }
+                else if (cmd == "Д")
+                {
+                    break;
+                }
+                else
+                {
+                    return;
+                }
+            }
+        }
+    }
+}
